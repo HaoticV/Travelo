ae167bcbcf2979ba5f22c07138f9028355c105e6
Jacek Boguta <jacekboguta@gmail.com>
2019-12-26 09:24:57 +0100

    modules sync

--- app/src/main/java/com/example/travelo/profile/ProfileActivityNew.kt
+++ app/src/main/java/com/example/travelo/profile/ProfileActivityNew.kt
@@ -3,8 +3,7 @@ package com.example.travelo.profile
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,13 +15,14 @@ import com.example.travelo.models.Route
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.jaeger.library.StatusBarUtil
 import kotlinx.android.synthetic.main.activity_profile_toolbar_collapse.*
 
 class ProfileActivityNew : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.TransparentStatusBar)
         setContentView(R.layout.activity_profile_toolbar_collapse)
+        StatusBarUtil.setTransparent(this)
         initToolbar()
         profile_collapsing_routes_fab.setOnClickListener { initRoutesRecyclerView() }
     }
