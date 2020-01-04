f9afd20349a3563794afb73d2a54b458b2807523
Krzysztof <k.ciolek93@gmail.com>
2020-01-04 15:56:30 +0100

    lista tras wyświetlana w profilu użytkownika bez opisu

--- app/src/main/java/com/example/travelo/profile/RouteRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/travelo/profile/RouteRecyclerViewAdapter.kt
@@ -4,24 +4,39 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelo.R
 import com.example.travelo.lib.ItemAnimation
 import com.example.travelo.models.Route
-import kotlinx.android.synthetic.main.item_people_chat.view.*
+import kotlinx.android.synthetic.main.item_profile_route.view.*
 
-class RouteRecyclerViewAdapter(val context: Context, private val items: ArrayList<Route>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RouteRecyclerViewAdapter(val context: Context, private val items: ArrayList<Route>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_people_chat, parent, false)
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_profile_route, parent, false)
         return RouteRecyclerViewAdapterVH(v)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = holder.itemView
         item.name.text = items[position].name
+        item.route_distance.text = items[position].distanceText
+        item.route_time.text = items[position].timeText
+
+        if (items[position].type.equals("mountain")) {
+            item.image.setImageResource(R.drawable.ic_cyclist_mountain)
+        }
+        if (items[position].type.equals("city")) {
+            item.image.setImageResource(R.drawable.ic_cyclist_city)
+        }
+        if (items[position].type.equals("road")) {
+            item.image.setImageResource(R.drawable.ic_cyclist_road)
+        }
         setAnimation(holder.itemView, position)
     }
 
