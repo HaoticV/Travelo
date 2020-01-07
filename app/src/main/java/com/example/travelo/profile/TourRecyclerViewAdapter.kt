c9a581d5c7a2defddef91966b4269c6c8d3a222b
Jacek Boguta <jacekboguta@gmail.com>
2020-01-07 05:39:57 +0100

    mam dość

--- app/src/main/java/com/example/travelo/profile/TourRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/travelo/profile/TourRecyclerViewAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.travelo.profile
 
 import android.content.Context
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,42 +18,59 @@ class TourRecyclerViewAdapter(val context: Context, private val items: ArrayList
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
+    private val TOUR_VIEW = 1
+    private val SECTION_VIEW = 0
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_profile_tour, parent, false)
-        return TourRecyclerViewAdapterVH(v)
+        if (viewType == TOUR_VIEW) {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.item_profile_tour, parent, false)
+            return TourRecyclerViewAdapterVH(v)
+        } else {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.item_section, parent, false)
+            return SectionViewHolder(v)
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = holder.itemView
-        item.tour_name.text = items[position].first.name
+        if (items[position].first is Tour) {
+            val tour = (items[position].first) as Tour
+            val route = (items[position].second) as Route
+            @Suppress("UNCHECKED_CAST") val user = (items[position].third) as ArrayList<User>
 
-        items[position].third.forEach {
-            if (items[position].first.host.equals(it.id)) {
-                Glide.with(context).load(it.image).into(item.host_profile_image)
-            }
-        }
+            item.tour_name.text = tour.name
 
-        item.tour_date.text = items[position].first.date
-        item.tour_date_time.text = items[position].first.dateTime
-        item.number_of_participants.text = "Liczba uczestników: " + items[position].third.size.toString()
-        item.route_name.text = items[position].second.name
-        when (items[position].second.type) {
-            "mountain" -> {
-                item.ic_route_type.setImageResource(R.drawable.ic_cyclist_mountain)
-                item.route_type.text = "Trasa górska"
+            user.forEach {
+                if (tour.host == it.id) {
+                    Glide.with(context).load(it.image).into(item.host_profile_image)
+                }
             }
-            "city" -> {
-                item.ic_route_type.setImageResource(R.drawable.ic_cyclist_city)
-                item.route_type.text = "Trasa miejska"
+
+            item.tour_date.text = tour.date
+            item.tour_date_time.text = tour.dateTime
+            item.number_of_participants.text = "Liczba uczestników: " + user.size.toString()
+            item.route_name.text = route.name
+
+            when (route.type) {
+                "mountain" -> {
+                    item.ic_route_type.setImageResource(R.drawable.ic_cyclist_mountain)
+                    item.route_type.text = "Trasa górska"
+                }
+                "city" -> {
+                    item.ic_route_type.setImageResource(R.drawable.ic_cyclist_city)
+                    item.route_type.text = "Trasa miejska"
+                }
+                "road" -> {
+                    item.ic_route_type.setImageResource(R.drawable.ic_cyclist_road)
+                    item.route_type.text = "Trasa szosowa"
+                }
             }
-            "road" -> {
-                item.ic_route_type.setImageResource(R.drawable.ic_cyclist_road)
-                item.route_type.text = "Trasa szosowa"
-            }
+            item.route_time.text = route.timeText
+            item.route_distance.text = route.distanceText
+        } else {
+            val view: SectionViewHolder = holder as SectionViewHolder
+            view.sectionTitle.text = items[position].toString()
         }
-        item.route_time.text = items[position].second.timeText
-        item.route_distance.text = items[position].second.distanceText
 
         setAnimation(holder.itemView, position)
     }
@@ -73,8 +89,19 @@ class TourRecyclerViewAdapter(val context: Context, private val items: ArrayList
         return items.size
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if (items[position].first is Tour)
+            return TOUR_VIEW
+        else
+            return SECTION_VIEW
+    }
+
     class TourRecyclerViewAdapterVH(v: View) : RecyclerView.ViewHolder(v) {
         var name: TextView = v.findViewById<View>(R.id.tour_name) as TextView
 
+    }
+
+    class SectionViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        var sectionTitle: TextView = v.findViewById(R.id.title_section)
     }
 }
